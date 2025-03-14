29b470dc8b1cd6e9efe2ba1ac04904692a208f1b
Ahsan Ali <ahsan326ali@gmail.com>
2025-03-14 20:27:35 +0500

    feat: search history

--- src/pages/SearchHistory/SearchHistory.jsx
+++ src/pages/SearchHistory/SearchHistory.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getSearchHistory } from '../../_core/features/historySlice'
+import { useLocation } from 'react-router-dom'
 
 const SearchHistory = () => {
+  const location= useLocation()
+  const dispatch = useDispatch()
+  const {SearchHistory,isSearchHistoryLoading,errorSearchHistory}=useSelector((state)=>state.history)
+  const userData = useSelector((state) => state.auth.userData)
+  useEffect(() => {
+    if (userData?.token) {
+      dispatch(
+        getSearchHistory(userData.token)
+      )
+    }
+  }, [dispatch, userData?.token],location.state)
+  console.log("searchHistory data",SearchHistory)
   return (
     <div>SearchHistory</div>
   )
