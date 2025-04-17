c4826678ca22ceb4c2a26fdbdb269e87ee51cfca
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-04-17 23:04:05 +0500

    Feat: base url change

--- src/utils/ApiBaseUrl.js
+++ src/utils/ApiBaseUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
-// const BASE_URL = "https://api.skyight.com";
+// const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://api.skyight.com";
 
 export { BASE_URL };
