384a3e0643293925a17daf9a5c5b25a3ffa09ea0
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-04-18 05:29:56 +0500

    Feat: change base URL for live

--- src/utils/ApiBaseUrl.js
+++ src/utils/ApiBaseUrl.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000"
-// const BASE_URL = "https://api.skyight.com";
+// const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://api.skyight.com";
 // const BASE_URL = "https://g4n8vxxb-3000.inc1.devtunnels.ms";
 
 export { BASE_URL };
