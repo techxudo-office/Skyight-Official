e3e13171598001edf6bfb5619682cbc19ec45d97
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-04-23 07:44:48 +0500

    Fix: active routes issue fixed

--- src/pages/SearchFlights/SearchFlights.jsx
+++ src/pages/SearchFlights/SearchFlights.jsx
@@ -10,32 +10,24 @@ import {
   CardLayoutFooter,
 } from "../../components/CardLayout/CardLayout";
 import {
-  Select,
   Button,
   CustomDate,
   MultiCity,
   Spinner,
 } from "../../components/components";
-import { iranianCities } from "../../data/iranianCities";
 import { FaPlaneDeparture } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";
 import { Formik, Form } from "formik";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import { FaPlaneDeparture } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
-import { iranianCities } from "../../data/iranianCities";
 import { FlightsBanner, forBackArrows } from "../../assets/Index";
-import { internationalCities } from "../../data/InternationalCities";
 import RadioButtons from "../../components/RadioButtons/RadioButtons";
-import { Button, CustomDate, MultiCity } from "../../components/components";
 import {
   getBookingRoutes,
   searchFlight,
   setSearchForm,
 } from "../../_core/features/bookingSlice";
 import { searchFlightValidationSchema } from "../../schema/validationSchema";
-import { searchFlight, setSearchForm } from "../../_core/features/bookingSlice";
 import {
   adultOptions,
   cabinClassOptions,
@@ -53,16 +45,6 @@ const SearchFlights = ({ OnlySearch, onSearch }) => {
   const { isLoadingSearchResults, searchForm, bookingRoutes, isLoadingRoutes } =
     useSelector((state) => state.booking);
   const [activeField, setActiveField] = useState(initialState);
-
-  const getCityOptions = (
-    type,
-    exclude,
-    iranianCities,
-    internationalCities
-  ) => {
-    const cities = type === "Domestic" ? iranianCities : internationalCities;
-    return cities.filter((item) => item.value !== exclude);
-  };
 
   const activateField = (fieldName) => {
     setActiveField((prev) => {
@@ -235,7 +217,6 @@ const SearchFlights = ({ OnlySearch, onSearch }) => {
                   tripType: "One-Way",
                 }));
               }, [values.flightRoute]);
-
               return (
                 <Form>
                   <CardLayoutBody>
@@ -270,153 +251,134 @@ const SearchFlights = ({ OnlySearch, onSearch }) => {
                       </div>
                     )}
                   </CardLayoutBody>
-
-                  {values.tripType !== "Multi-City" ? (
-                    <>
-                      <CardLayoutBody>
-                        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-5 mb-7">
-                          <div className="relative flex flex-col mb-5 md:flex-row max-md:items-center">
-                            <FormSelect
-                              id="departure"
-                              label="Departure From"
-                              options={
-                                // !values.arrival ? bookingRoutes?.map(route => {
-                                //   console.log(route, "route")
-                                //   return ({
-                                //     value: route.Origin,
-                                //     label: route.Origin
-                                //   })
-                                // }) :
-                                bookingRoutes?.map((route) => {
-                                  console.log(route, "route");
-                                  return {
-                                    value: route.Origin,
-                                    label: route.Origin,
-                                  };
-                                })
-                              }
-                              value={values.departure}
-                              onChange={(option) => {
-                                setFieldValue("departure", option.value);
-                                activateField("arrival");
-                              }}
-                              touched={touched.departure}
-                              error={errors.departure}
-                              icons={<FaPlaneDeparture />}
-                            />
-                            <div className="rounded-full border-gray border-[1px] w-fit h-fit pt-3 pl-3 p-2 z-10 -mt-3 md:-ml-3 md:mt-2 -mb-11 md:-mr-8 bg-white">
-                              <img
-                                src={forBackArrows}
-                                alt=""
-                                className="w-8 p-0 bg-white md:w-6"
-                              />
+                  <CardLayoutBody>
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-5 mb-7">
+                      <div className="relative flex flex-col mb-5 md:flex-row max-md:items-center">
+                        <FormSelect
+                          id="departure"
+                          label="Departure From"
+                          options={bookingRoutes?.map((route) => ({
+                            value: route.Origin,
+                            label: route.Origin,
+                          }))}
+                          value={values.departure}
+                          onChange={(option) => {
+                            setFieldValue("departure", option.value);
+                            activateField("arrival");
+                          }}
+                          touched={touched.departure}
+                          error={errors.departure}
+                          icons={<FaPlaneDeparture />}
+                        />
+                        <div className="rounded-full border-gray border-[1px] w-fit h-fit pt-3 pl-3 p-2 z-10 -mt-3 md:-ml-3 md:mt-2 -mb-11 md:-mr-8 bg-white">
+                          <img
+                            src={forBackArrows}
+                            alt=""
+                            className="w-8 p-0 bg-white md:w-6"
+                          />
+                        </div>
+                      </div>
+                      <FormSelect
+                        id="arrival"
+                        label="Arrival To"
+                        className={"select w-full"}
+                        isSelected={activeField.arrival}
+                        isLoading={isLoadingRoutes}
+                        disabled={!values.departure}
+                        options={bookingRoutes
+                          ?.filter((route) => route.Origin === values.departure)
+                          .map((route) => ({
+                            value: route.Destination,
+                            label: route.Destination,
+                          }))}
+                        value={values.arrival}
+                        placeholder="Select Arrival"
+                        onChange={(option) => {
+                          setFieldValue("arrival", option.value);
+                          activateField("departureDate");
+                        }}
+                        optionIcons={<FaPlaneArrival />}
+                        selectIcon={<FaPlaneArrival size={18} />}
+                      />
+                      <div className="relative mb-5 select">
+                        <CustomDate
+                          onChange={(e) => {
+                            setFieldValue("departureDate", e.target.value);
+                            activateField(
+                              values.tripType !== "One-Way"
+                                ? "returnDate"
+                                : "adult"
+                            );
+                          }}
+                          value={values.departureDate}
+                          isSelected={activeField.departureDate}
+                          name="departureDate"
+                          label="Departure Date"
+                          pastDate={false}
+                        />
+                        {touched.departureDate && errors.departureDate && (
+                          <div className="absolute left-0 mt-2 text-sm text-red-500">
+                            {errors.departureDate}
+                          </div>
+                        )}
+                      </div>
+                      <div className="relative mb-5 select">
+                        <CustomDate
+                          onChange={(e) => {
+                            setFieldValue("returnDate", e.target.value);
+                            activateField("adult");
+                          }}
+                          value={
+                            values.tripType === "One-Way"
+                              ? ""
+                              : values.returnDate
+                          }
+                          isSelected={activeField.returnDate}
+                          name="returnDate"
+                          label="Return Date"
+                          disabled={values.tripType === "One-Way"}
+                          pastDate={false}
+                        />
+                        {values.tripType !== "One-Way" &&
+                          touched.returnDate &&
+                          errors.returnDate && (
+                            <div className="absolute left-0 mt-2 text-sm text-red-500">
+                              {errors.returnDate}
                             </div>
-                          </div>
-                          <FormSelect
-                            id="arrival"
-                            label="Arrival To"
-                            className={"select w-full"}
-                            isSelected={activeField.arrival}
-                            isLoading={isLoadingRoutes}
-                            options={bookingRoutes?.map((route) => {
-                              console.log(route, "route");
-                              return {
-                                value: route.Destination,
-                                label: route.Destination,
-                              };
-                            })}
-                            value={values.arrival}
-                            placeholder="Select Arrival"
-                            onChange={(option) => {
-                              setFieldValue("arrival", option.value);
-                              activateField("departureDate");
-                            }}
-                            optionIcons={<FaPlaneArrival />}
-                            selectIcon={<FaPlaneArrival size={18} />}
-                          />
-                          <div className="relative mb-5 select">
-                            <CustomDate
-                              onChange={(e) => {
-                                setFieldValue("departureDate", e.target.value);
-                                activateField(
-                                  values.tripType !== "One-Way"
-                                    ? "returnDate"
-                                    : "adult"
-                                );
-                              }}
-                              value={values.departureDate}
-                              isSelected={activeField.departureDate}
-                              name="departureDate"
-                              label="Departure Date"
-                              pastDate={false}
-                            />
-                            {touched.departureDate && errors.departureDate && (
-                              <div className="absolute left-0 mt-2 text-sm text-red-500">
-                                {errors.departureDate}
-                              </div>
-                            )}
-                          </div>
-                          <div className="relative mb-5 select">
-                            <CustomDate
-                              onChange={(e) => {
-                                setFieldValue("returnDate", e.target.value);
-                                activateField("adult");
-                              }}
-                              value={
-                                values.tripType === "One-Way"
-                                  ? ""
-                                  : values.returnDate
-                              }
-                              isSelected={activeField.returnDate}
-                              name="returnDate"
-                              label="Return Date"
-                              disabled={values.tripType === "One-Way"}
-                              pastDate={false}
-                            />
-                            {values.tripType !== "One-Way" &&
-                              touched.returnDate &&
-                              errors.returnDate && (
-                                <div className="absolute left-0 mt-2 text-sm text-red-500">
-                                  {errors.returnDate}
-                                </div>
-                              )}
-                          </div>
-                          {selectFields.map(
-                            ({ id, label, options, icon, next }) => (
-                              <FormSelect
-                                key={id}
-                                id={id}
-                                label={label}
-                                options={options}
-                                value={values[id]}
-                                onChange={(option) => {
-                                  setFieldValue(id, option.value);
-                                  if (next) activateField(next);
-                                }}
-                                touched={touched[id]}
-                                error={errors[id]}
-                                isSelected={activeField[id]}
-                                icons={icon}
-                              />
-                            )
                           )}
-                        </div>
-                      </CardLayoutBody>
-                      <CardLayoutFooter>
-                        <div onClick={onSearch}>
-                          <Button
-                            icon={<MdSearch />}
-                            text="Search Flight"
-                            type="submit"
-                            disabled={isLoadingSearchResults}
-                            loading={isLoadingSearchResults}
+                      </div>
+                      {selectFields.map(
+                        ({ id, label, options, icon, next }) => (
+                          <FormSelect
+                            key={id}
+                            id={id}
+                            label={label}
+                            options={options}
+                            value={values[id]}
+                            onChange={(option) => {
+                              setFieldValue(id, option.value);
+                              if (next) activateField(next);
+                            }}
+                            touched={touched[id]}
+                            error={errors[id]}
+                            isSelected={activeField[id]}
+                            icons={icon}
                           />
-                        </div>
-                      </CardLayoutFooter>
-                    </>
-                  ) : (
-                    <MultiCity />
-                  )}
+                        )
+                      )}
+                    </div>
+                  </CardLayoutBody>
+                  <CardLayoutFooter>
+                    <div onClick={onSearch}>
+                      <Button
+                        icon={<MdSearch />}
+                        text="Search Flight"
+                        type="submit"
+                        disabled={isLoadingSearchResults}
+                        loading={isLoadingSearchResults}
+                      />
+                    </div>
+                  </CardLayoutFooter>
                 </Form>
               );
             }}
