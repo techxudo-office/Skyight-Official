6917d5273940636ae6ebb658ec9140a5778b62b5
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-04-24 13:17:43 +0500

    Fix: prices shown in pkr and added pkr

--- src/components/FlightCard/FlightCard.jsx
+++ src/components/FlightCard/FlightCard.jsx
@@ -39,8 +39,8 @@ const FlightCard = ({ data, doc_type, travelers, pricingInfo }) => {
     <>
       <CardLayoutContainer className={"mb-5"}>
         <CardLayoutHeader className={""}>
-          <div className="text-text text-sm mb-3">
-            <h2 className=" font-semibold text-text">
+          <div className="mb-3 text-sm text-text">
+            <h2 className="font-semibold text-text">
               {data.AirItinerary.OriginDestinationOptions.length === 1 ?
                 "Non Stop (fix)" : `Stops: ${data.AirItinerary.OriginDestinationOptions.length}`
               }
@@ -49,7 +49,7 @@ const FlightCard = ({ data, doc_type, travelers, pricingInfo }) => {
           {flightSegment.map((flights, idx) => (
             <div
               key={idx}
-              className="w-full mb-4 flex flex-wrap gap-5 justify-between items-center">
+              className="flex flex-wrap items-center justify-between w-full gap-5 mb-4">
               <div>
                 <h2 className="text-xl font-semibold text-text">
                   {flights.OperatingAirline.Code}
@@ -69,23 +69,23 @@ const FlightCard = ({ data, doc_type, travelers, pricingInfo }) => {
                 </div>
                 <div className="flex items-center justify-center gap-5">
                   <div className="flex flex-col items-center justify-center">
-                    <h2 className="text-md font-semibold text-text">
+                    <h2 className="font-semibold text-md text-text">
                       {flights.DepartureAirport.LocationCode}
                     </h2>
-                    <h2 className="text-md font-semibold text-text">
+                    <h2 className="font-semibold text-md text-text">
                       {flights.DepartureTime}
                     </h2>
                   </div>
-                  <div className="flex gap-3 items-center text-primary">
+                  <div className="flex items-center gap-3 text-primary">
                     <span className="h-0.5 w-8 bg-primary"></span>
                     <IoIosAirplane className="text-2xl" />
                     <span className="h-0.5 w-8 bg-primary"></span>
                   </div>
                   <div className="flex flex-col items-center justify-center">
-                    <h2 className="text-md font-semibold text-text">
+                    <h2 className="font-semibold text-md text-text">
                       {flights.ArrivalAirport.LocationCode}
                     </h2>
-                    <h2 className="text-md font-semibold text-text">
+                    <h2 className="font-semibold text-md text-text">
                       {flights.ArrivalTime}
                     </h2>
                   </div>
@@ -106,7 +106,7 @@ const FlightCard = ({ data, doc_type, travelers, pricingInfo }) => {
                   className={`flex justify-between items-center py-3 ${index == totalTravelers.length - 1 ? "" : "border-b"
                     }  border-slate-200`}>
                   <div>
-                    <h2 className="text-sm font-semibold text-text flex items-center  gap-1">
+                    <h2 className="flex items-center gap-1 text-sm font-semibold text-text">
                       <span className="text-lg">
                         {item == "adult" ? (
                           <MdPerson />
@@ -123,7 +123,7 @@ const FlightCard = ({ data, doc_type, travelers, pricingInfo }) => {
                     {flightSegment.map((item, idx) => (
                       <h2
                         key={idx}
-                        className="text-sm mb-1 font-semibold text-text  flex items-center gap-1">
+                        className="flex items-center gap-1 mb-1 text-sm font-semibold text-text">
                         <span>
                           <FaSuitcase />
                         </span>
@@ -136,21 +136,21 @@ const FlightCard = ({ data, doc_type, travelers, pricingInfo }) => {
                     ))}
                   </div>
 
-                  <h2 className="text-sm font-semibold text-text flex items-center justify-end gap-1">
+                  <h2 className="flex items-center justify-end gap-1 text-sm font-semibold text-text">
                     <span>
                       <FaMoneyBillAlt />
                     </span>
-                    <span className="">{(pricingInfo.PTC_FareBreakdowns[index].PassengerFare.TotalFare.pkrTotalFare).toLocaleString()}</span>
+                    <span className="">{(pricingInfo.PTC_FareBreakdowns[index].PassengerFare.TotalFare.pkrTotalFare).toLocaleString()} PKR</span>
                   </h2>
                 </div>
               ))}
           </div>
         </CardLayoutBody>
         <CardLayoutFooter>
-          <div className=" mr-4 lg:mr-7">
-            <h1 className="text-text text-xl font-semibold">Total:{" "}
+          <div className="mr-4 lg:mr-7">
+            <h1 className="text-xl font-semibold text-text">Total:{" "}
               <span className="text-primary">
-                {(pricingInfo.ItinTotalFare.TotalFare.pkrTotalFare).toLocaleString()}
+                {(pricingInfo.ItinTotalFare.TotalFare.pkrTotalFare).toLocaleString()} PKR
               </span>
             </h1>
           </div>
