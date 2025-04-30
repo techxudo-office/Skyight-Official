538d4fc0e9519b9c2da3580bbfcc73d0b691e4b5
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-04-30 22:30:20 +0500

    Feat: protected routing optimized

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { Sidebar, Header, Backbutton, Modal } from "../components/components";
 import { logo } from "../assets/Index";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
 
 const Layout = () => {
   const [sidebarStatus, setSidebarStatus] = useState(true);
-  // const [ModalStatus, setModalStatus] = useState(false);
+  const { userData } = useSelector((state) => state.auth);
+  const location = useLocation();
 
   const setSidebarStatusHandler = (status) => {
     setSidebarStatus(status);
   };
+
+  if (!userData?.token) {
+    toast.success("Logged out successfully");
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
   return (
     <>
@@ -18,27 +26,26 @@ const Layout = () => {
         setSidebarStatusHandler={setSidebarStatusHandler}
       />
       <div className="flex h-screen">
-        {/* <Modal
+        {/* 
+        <Modal
           title={'Session Expired'}
           imgsrc={logo}
           btnText={'signup / login'}
-          Message={' Your session has expired. Please sign up or log in again to continue.'}
+          Message={'Your session has expired. Please sign up or log in again to continue.'}
           active={ModalStatus}
           onClose={() => setModalStatus(false)}
           toggle={true}
-        /> */}
+        /> 
+        */}
         <Sidebar status={sidebarStatus} updateStatus={setSidebarStatus} />
         <div className="flex-1 w-full md:w-4/5">
-
           <div
-            className="flex flex-col justify-between items-center h-[100vh]  bg-slate-100 overflow-scroll"
+            className="flex flex-col justify-between items-center h-[100vh] bg-slate-100 overflow-scroll"
             style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
           >
-
-            <div className="flex flex-col items-center  justify-center w-full px-4 pt-28">
+            <div className="flex flex-col items-center justify-center w-full px-4 pt-28">
               <Outlet />
             </div>
-
           </div>
         </div>
       </div>
