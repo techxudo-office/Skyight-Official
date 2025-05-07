d291d9cc3837f9e613065164e82251d45cbb0f6d
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-05-07 10:13:52 +0500

    Fix: layout cleanup

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -8,7 +8,6 @@ const Layout = () => {
   const [sidebarStatus, setSidebarStatus] = useState(true);
   const [headline, setHeadline] = useState(true);
   const { userData } = useSelector((state) => state.auth);
-  // const [ModalStatus, setModalStatus] = useState(false);
 
   const setSidebarStatusHandler = (status) => {
     setSidebarStatus(status);
@@ -27,24 +26,13 @@ const Layout = () => {
         setSidebarStatusHandler={setSidebarStatusHandler}
       />
       <div className="flex h-screen">
-        {/* 
-        <Modal
-          title={'Session Expired'}
-          imgsrc={logo}
-          btnText={'signup / login'}
-          Message={'Your session has expired. Please sign up or log in again to continue.'}
-          active={ModalStatus}
-          onClose={() => setModalStatus(false)}
-          toggle={true}
-        /> 
-        */}
         <Sidebar status={sidebarStatus} updateStatus={setSidebarStatus} />
         <div className="flex-1 w-full md:w-4/5">
           <div
             className="flex flex-col justify-between items-center h-[100vh] bg-slate-100 overflow-scroll"
             style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
           >
-            <div className="flex flex-col items-center  justify-center w-full px-4 pt-28">
+            <div className="flex flex-col items-center justify-center w-full px-4 pt-28">
 
               <Outlet />
             </div>
